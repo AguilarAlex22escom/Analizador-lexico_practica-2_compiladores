359276152bcb61280ec13f4e4ff8c254a9c6f3a1
Ramírez Aguilar Marc Alexander <mramireza1700@alumno.ipn.mx>
2022-10-29 18:49:45 -0500

    Versión 1: Pasos iniciales

---
+++ lib/expresiones_regulares.py
@@ -0,0 +1,30 @@
+import re 
+
+# Datos.
+
+lexema_identificador = re.compile('(\w)+(\w)*?')
+lexema_numerico = re.compile('([0-9])+(".")?([0-9])*?')
+lexema_booleano = re.compile('true|false')
+lexema_caracter = re.compile("'(lexema_identificador{1})?'|' '")
+lexema_cadena = re.compile('"lexema_identificador{2,}"')
+
+# Operadores.
+
+lexema_comparacion = re.compile('=|==|!=|<|>|<=|>=|===')
+lexema_incremento = re.compile('(lexema_identificador)(++|--)')
+lexema_matematico = re.compile('+|-|"*"|"**"|/|%')
+lexema_logico = re.compile('&&|"||"|!')
+lexema_asignacion = re.compile('+=|-=|*=|/=')
+
+# Palabras reservadas.
+
+lexema_tipoDeDato = re.compile('void|int|float|double|char')
+lexema_calificador = re.compile('short|long|signed|unsigned')
+# lexema_tipoDeDato = re.compile('(void|int|float|double|char) lexema_identificador (=" *"?(lexema_cadena|lexema_numerico))?')
+lexema_if = re.compile('if" *"?(((lexema_identificador)(==|<|>|<=|>=|===|!=)(lexema_identificador))|(lexema_numerico))')
+lexema_else = re.compile('else" *"?(lexema_if)?')
+lexema_for = re.compile('for" *"?(((lexema_identificador)=(lexema_identificador))?;((lexema_identificador)(<|>|<=|>=|===|!=)(lexema_identificador))?;((lexema_incremento)))')
+lexema_while = re.compile('while" *"?(((lexema_identificador)(lexema_comparacion)(lexema_identificador))|(lexema_numerico))')
+lexema_switch = re.compile('switch" *"?(lexema_identificador)')
+
+# Símbolos especiales
---
+++ practica2.py
@@ -0,0 +1,20 @@
+from ast import pattern
+from lib import expresiones_regulares
+import re
+
+def leerArchivos():
+    archivos = input("Ingrese el programa a análizar: ") # "el archivo donde se guardará la tabla de símbolos y el archivo donde se encontrarán los errores: ")    # En la primer versión del programa solo se trabaja para leer el programa en c.    
+    FILE = open(archivos, 'r') # Apertura del archivo en modo lectura.
+    print(FILE.readable()) # Indica si se puede abrir el archivo en modo lectura.
+    # print(FILE.read()) # Convierte el contenido del archivo en una cadena.
+    # print(FILE.readlines()) # Guarda cada línea del contenido del archivo en un índice de una lista.
+    for line in FILE:
+        print(line) # Imprime el programa línea por línea.
+    FILE.close() # Cierra el archivo.
+
+leerArchivos()
+
+# Textos de referencia:
+
+# https://www.freecodecamp.org/espanol/news/python-abre-archivo-como-leer-un-archivo-de-texto-linea-por-linea/
+# https://platzi.com/blog/expresiones-regulares-python/
