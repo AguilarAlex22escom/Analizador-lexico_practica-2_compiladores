74537223895a4f4dc6e56baf616675d9fc33b12a
Ramírez Aguilar Marc Alexander <mramireza1700@alumno.ipn.mx>
2022-10-31 22:14:41 -0600

    Segunda versión con análisis de ReGex.

--- lib/expresiones_regulares.py
+++ lib/expresiones_regulares.py
@@ -1,30 +1,40 @@
 import re 
 
+lexema_identificador = re.compile('.+')
+lexema_tipoDeDato = re.compile('void\s.+|int\s.+|float\s.+|double\s.+|char\s.+')
+
+# cadena_prueba = 'int main()'
+# matcher = lexema_tipoDeDato.match('int main()')
+
+m = 0
+for m in lexema_tipoDeDato.finditer('int main()'):
+    print(m)
+#print lexema_tipoDeDato.group(1))
 # Datos.
 
-lexema_identificador = re.compile('(\w)+(\w)*?')
-lexema_numerico = re.compile('([0-9])+(".")?([0-9])*?')
-lexema_booleano = re.compile('true|false')
-lexema_caracter = re.compile("'(lexema_identificador{1})?'|' '")
-lexema_cadena = re.compile('"lexema_identificador{2,}"')
+# lexema_identificador = re.compile('(\w)+(\w)*?')
+# lexema_numerico = re.compile('([0-9])+(".")?([0-9])*?')
+# lexema_booleano = re.compile('true|false')
+# lexema_caracter = re.compile("'(lexema_identificador{1})?'|' '")
+# lexema_cadena = re.compile('"lexema_identificador{2,}"')
 
-# Operadores.
+# # Operadores.
 
-lexema_comparacion = re.compile('=|==|!=|<|>|<=|>=|===')
-lexema_incremento = re.compile('(lexema_identificador)(++|--)')
-lexema_matematico = re.compile('+|-|"*"|"**"|/|%')
-lexema_logico = re.compile('&&|"||"|!')
-lexema_asignacion = re.compile('+=|-=|*=|/=')
+# lexema_comparacion = re.compile('=|==|!=|<|>|<=|>=|===')
+# lexema_incremento = re.compile('(lexema_identificador)(++|--)')
+# lexema_matematico = re.compile('+|-|"*"|"**"|/|%')
+# lexema_logico = re.compile('&&|"||"|!')
+# lexema_asignacion = re.compile('+=|-=|*=|/=')
 
-# Palabras reservadas.
+# # Palabras reservadas.
 
-lexema_tipoDeDato = re.compile('void|int|float|double|char')
-lexema_calificador = re.compile('short|long|signed|unsigned')
-# lexema_tipoDeDato = re.compile('(void|int|float|double|char) lexema_identificador (=" *"?(lexema_cadena|lexema_numerico))?')
-lexema_if = re.compile('if" *"?(((lexema_identificador)(==|<|>|<=|>=|===|!=)(lexema_identificador))|(lexema_numerico))')
-lexema_else = re.compile('else" *"?(lexema_if)?')
-lexema_for = re.compile('for" *"?(((lexema_identificador)=(lexema_identificador))?;((lexema_identificador)(<|>|<=|>=|===|!=)(lexema_identificador))?;((lexema_incremento)))')
-lexema_while = re.compile('while" *"?(((lexema_identificador)(lexema_comparacion)(lexema_identificador))|(lexema_numerico))')
-lexema_switch = re.compile('switch" *"?(lexema_identificador)')
+# lexema_tipoDeDato = re.compile('void|int|float|double|char')
+# lexema_calificador = re.compile('short|long|signed|unsigned')
+# # lexema_tipoDeDato = re.compile('(void|int|float|double|char) lexema_identificador (=" *"?(lexema_cadena|lexema_numerico))?')
+# lexema_if = re.compile('if" *"?(((lexema_identificador)(==|<|>|<=|>=|===|!=)(lexema_identificador))|(lexema_numerico))')
+# lexema_else = re.compile('else" *"?(lexema_if)?')
+# lexema_for = re.compile('for" *"?(((lexema_identificador)=(lexema_identificador))?;((lexema_identificador)(<|>|<=|>=|===|!=)(lexema_identificador))?;((lexema_incremento)))')
+# lexema_while = re.compile('while" *"?(((lexema_identificador)(lexema_comparacion)(lexema_identificador))|(lexema_numerico))')
+# lexema_switch = re.compile('switch" *"?(lexema_identificador)')
 
 # Símbolos especiales
--- practica2.py
+++ practica2.py
@@ -18,3 +18,4 @@ leerArchivos()
 
 # https://www.freecodecamp.org/espanol/news/python-abre-archivo-como-leer-un-archivo-de-texto-linea-por-linea/
 # https://platzi.com/blog/expresiones-regulares-python/
+# https://platzi.com/contributions/entendiendo-las-expresiones-regulares-con-python/
